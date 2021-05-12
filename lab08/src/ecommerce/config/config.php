19b49c261b0b5d40f4f6004e0dddccb04b682ab2
erikha1501 <maskdroid01@gmail.com>
2021-05-12 17:27:31 +0700

    Containerize lab08

--- lab08/src/ecommerce/config/config.php
+++ lab08/src/ecommerce/config/config.php
@@ -10,7 +10,7 @@ define('DB_USER', 'mysql_client');
 define('DB_PASSWORD', 'p');
 define('DB_HOST', 'db');
 
-define('BASE_PATH','http://localhost/wplab-20202/lab08/framework');
+define('BASE_PATH','http://localhost:6969/ecommerce');
 
 
 define('PAGINATE_LIMIT', '5');
--- lab08/src/ecommerce/db/framework-20090330111635.sql
+++ lab08/src/ecommerce/db/framework-20090330111635.sql
@@ -1,4 +1,3 @@
-
 USE framework;
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
